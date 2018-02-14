0414eb2175eae7fce30228b29e87ffe648f7ef30
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2018-02-14 14:00:20 -0600

    add a sample program better system err versus system out settings use a retryer library instead of default

---
+++ src/main/java/com/lucidworks/sso/CallSSOCookieFetcher.java
@@ -0,0 +1,67 @@
+package com.lucidworks.sso;
+
+import org.apache.commons.io.FileUtils;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CallSSOCookieFetcher {
+  @Option(name = "--javaBin", usage = "The java executable path.")
+  private String java = "java";
+
+  @Option(name = "--jarFile", required = true, usage = "The SSOCookieFetcherJarFile")
+  private String jarFile;
+
+  public static void main(String [] args) throws Exception {
+    new CallSSOCookieFetcher().doMain(args);
+  }
+
+  public void doMain(String [] args) throws Exception {
+    CmdLineParser parser = new CmdLineParser(this);
+
+    parser.setUsageWidth(80);
+
+    parser.parseArgument(args);
+
+    ProcessBuilder builder = new ProcessBuilder(java, "-jar", jarFile, "--url", "https://fudgeunlimited.sharepoint.com/");
+    builder.environment().put("password", System.getenv("password"));
+    Process process = builder.start();
+
+    OutputStream stdin = process.getOutputStream();
+    InputStream stderr = process.getErrorStream();
+    InputStream stdout = process.getInputStream();
+
+    final List<String> stdoutList = new ArrayList<>();
+
+    new Thread(() -> {
+      Scanner scanner = new Scanner(stdout);
+      while (scanner.hasNextLine()) {
+        stdoutList.add(scanner.nextLine());
+      }
+      scanner.close();
+    }).start();
+
+    new Thread(() -> {
+      Scanner scanner = new Scanner(stderr);
+      while (scanner.hasNextLine()) {
+        System.out.println("STDERR: " + scanner.nextLine());
+      }
+      scanner.close();
+    }).start();
+
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+    writer.write(FileUtils.readFileToString(new File("input.json")));
+    writer.flush();
+
+    process.waitFor();
+  }
+}
--- src/main/java/com/lucidworks/sso/SSOCookieFetcher.java
+++ src/main/java/com/lucidworks/sso/SSOCookieFetcher.java
@@ -2,6 +2,12 @@ package com.lucidworks.sso;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.rholder.retry.Attempt;
+import com.github.rholder.retry.AttemptTimeLimiters;
+import com.github.rholder.retry.RetryListener;
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.RetryerBuilder;
+import com.github.rholder.retry.StopStrategies;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.ProxyConfig;
 import com.machinepublishers.jbrowserdriver.Settings;
@@ -18,9 +24,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.Console;
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 public class SSOCookieFetcher {
   private static ObjectMapper MAPPER = new ObjectMapper();
@@ -28,15 +34,17 @@ public class SSOCookieFetcher {
   @Option(name = "--url", required = true, usage = "Starting URL")
   private String url = null;
 
-  @Option(name = "--stepsPath", required = true, usage = "Steps json file path")
-  private String stepsJsonFile = null;
-
   @Option(name = "--proxy", usage = "Proxy server in <host>:<args> format")
   private String proxy = null;
 
   @Option(name = "--screenshots", usage = "Save screenshots of each web form as sso process proceeds")
   private boolean takeScreenshots = false;
 
+  @Option(name = "--stopAfterAttempt", usage = "Stop retrying to get sso cookies after failed attempt")
+  private int stopAfterAttempt = 5;
+
+  @Option(name = "--attemptTimeLimiter", usage = "Limit attempts to this many seconds")
+  private int attemptTimeLimiter = 5 * 60;
 
   public static void main(String[] args) throws Exception {
     new SSOCookieFetcher().runProcess(args);
@@ -50,30 +58,29 @@ public class SSOCookieFetcher {
 
     parser.parseArgument(args);
 
-    int retryCount = 6;
-    Exception failed = null;
-    while (retryCount-- > 0) {
-      try {
-        doSteps();
-        failed = null;
-        break;
-      } catch (Exception e) {
-        failed = e;
-        //System.out.println("Failed due to exception. Retries remaining " + retryCount);
-        //e.printStackTrace();
-      }
-    }
-    if (failed != null) {
-      failed.printStackTrace();
-      System.exit(1);
-    }
+    Retryer retryer = RetryerBuilder.newBuilder()
+        .withStopStrategy(StopStrategies.stopAfterAttempt(stopAfterAttempt))
+        .withAttemptTimeLimiter(AttemptTimeLimiters.fixedTimeLimit(attemptTimeLimiter, TimeUnit.SECONDS))
+        .retryIfException()
+        .retryIfRuntimeException()
+        .withRetryListener(new RetryListener() {
+          @Override
+          public <V> void onRetry(Attempt<V> attempt) {
+            if (attempt.hasException()) {
+              System.err.println("Error occurred during attempt");
+              attempt.getExceptionCause().printStackTrace(System.err);
+            }
+          }
+        }).build();
+    retryer.call(() -> {
+      doSteps();
+      return true;
+    });
   }
 
   private void doSteps() throws Exception {
-    JsonNode stepsJson = MAPPER.readTree(new File(stepsJsonFile));
-
+    JsonNode stepsJson = MAPPER.readTree(System.in);
     Settings.Builder settingsBuilder = Settings.builder();
-
     settingsBuilder.timezone(Timezone.AMERICA_NEWYORK);
 
     if (proxy != null) {
@@ -106,21 +113,20 @@ public class SSOCookieFetcher {
 
     settingsBuilder.ajaxWait(30000L);
     settingsBuilder.socketTimeout(30000);
-    settingsBuilder.blockAds(false);
-    settingsBuilder.quickRender(false);
+    settingsBuilder.blockAds(true);
+    settingsBuilder.quickRender(true);
     settingsBuilder.userAgent(UserAgent.CHROME);
-
     JBrowserDriver driver = new JBrowserDriver(settingsBuilder.build());
 
     try {
       driver.get(url);
 
+      System.err.println("Loaded " + url);
+
       driver.manage().window().setSize(new Dimension(800, 800));
 
       new WebDriverWait(driver, 60, 250).until(wd -> ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
-
       int screenshotIndex = 0;
-
       for (JsonNode nextStep : stepsJson) {
         ++screenshotIndex;
         if (nextStep.has("inputFields")) {
@@ -132,39 +138,44 @@ public class SSOCookieFetcher {
                 text = passwordEnv;
               } else {
                 Console console = System.console();
+                if (console == null) {
+                  System.err.println("Cannot get password and console is null");
+                  System.exit(1);
+                }
                 char passwordArray[] = console.readPassword("Enter password: ");
                 text = new String(passwordArray);
               }
             }
-            driver.findElement(By.xpath(inputField.get("xpath").asText())).clear();
+            String nextXpath = inputField.get("xpath").asText();
+            System.err.println("Entering field " + nextXpath);
+            driver.findElement(By.xpath(nextXpath)).clear();
             Thread.sleep(1500L);
-            driver.findElement(By.xpath(inputField.get("xpath").asText())).sendKeys(text);
+            driver.findElement(By.xpath(nextXpath)).sendKeys(text);
             Thread.sleep(1500L);
           }
         }
         if (nextStep.has("submitButtonXPath")) {
-
           if (takeScreenshots) {
             byte[] screenshot = driver.getScreenshotAs(OutputType.BYTES);
             Files.write(Paths.get("screenshot-beforeclick-" + screenshotIndex + ".png"), screenshot);
           }
-
+          System.err.println("Submitting button " + nextStep.get("submitButtonXPath").asText());
           driver.findElement(By.xpath(nextStep.get("submitButtonXPath").asText())).click();
           new WebDriverWait(driver, 60, 250).until(wd -> ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
-
           if (takeScreenshots) {
             byte[] screenshot = driver.getScreenshotAs(OutputType.BYTES);
             Files.write(Paths.get("screenshot-afterclick-" + screenshotIndex + ".png"), screenshot);
           }
-
         } else {
-          throw new Exception("Need a submit button for step " + nextStep.toString());
+          System.err.println("Need a submit button for step " + nextStep.toString());
+          System.exit(1);
         }
       }
       for (Cookie c : driver.manage().getCookies()) {
-        System.out.println(c.getName() + ";;;" + c.getValue());
+        System.out.println("COOKIE;;;" + c.getName() + ";;;" + c.getValue());
       }
     } finally {
+      System.err.println("Quitting jbrowserdriver process...");
       driver.quit();
     }
   }
